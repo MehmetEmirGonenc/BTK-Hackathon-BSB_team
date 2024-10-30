7fdd15199ba799094610b5672571f718027bdd4b
Serdar Altanay <serdaraltanay@gmail.com>
2024-10-30 22:15:17 +0300

    ne yaptığımı bilmiyorum

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,44 @@
 const express = require("express");
 const path = require("path");
 const upload = require('./utils/upload.js');
+const session = require('express-session');
 const { exec } = require("child_process");
 const { error } = require("console");
 const { stdout, stderr } = require("process");
+require('dotenv').config();
 
 const app = express();
 const PORT = 5000;
 
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { 
+      maxAge: 10 * 60 * 1000, // 10 dakika
+      httpOnly: true,         // JavaScript erişimini engeller
+      secure: false,          // HTTPS üzerinde aktif et, geliştirme için false
+      sameSite: 'lax'         // CSRF koruması
+    }
+  }));
+
+app.use((req, res, next) => {
+    if (!req.session) return next();
+  
+    // Oturum sona erdiğinde veya silindiğinde dosyayı sil
+    req.session.destroy(err => {
+      if (err) return next(err);
+      if (req.session.uploadedFile) {
+        const filePath = path.join(__dirname, 'temp', req.session.uploadedFile);
+        if (fs.existsSync(filePath)) {
+          fs.unlinkSync(filePath); // Dosyayı sil
+        }
+      }
+    });
+    next();
+  });
+
+//#region routes
 
 app.post("/summary", upload.single("file"), async(req,res,next) => {
     
